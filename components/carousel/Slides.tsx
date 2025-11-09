4475dc505dcead388ef75ba9954aefee62d307d4
Tom <tomor.enkh@gmail.com>
2025-11-09 15:58:33 +0800

    Texts and button of Slides component added

--- components/carousel/Slides.tsx
+++ components/carousel/Slides.tsx
@@ -8,8 +8,10 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { axiosInstance } from "@/lib/axios-instance";
+import { Play, Star } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
 
 export const Slides = () => {
   const [movies, setMovies] = useState<MovieDetail[]>([]);
@@ -27,7 +29,7 @@ export const Slides = () => {
   }, []);
 
   return (
-    <Carousel className="w-full bg-amber-300">
+    <Carousel className="w-full">
       <CarouselContent>
         {movies.map((movie, index) => {
           const imgUrl = `${process.env.NEXT_PUBLIC_TMDB_IMAGE_SERVICE_URL}/original${movie.backdrop_path}`;
@@ -49,6 +51,35 @@ export const Slides = () => {
       </CarouselContent>
       <CarouselPrevious className="left-11" />
       <CarouselNext className="right-11" />
+      <div className="left-[140px] top-[178px] absolute inline-flex flex-col justify-start items-start gap-4 w-[400px]">
+        <div>
+          <p className="text-white text-base font-normal leading-6">
+            Now Playing:
+          </p>
+          <h1 className="text-white text-4xl font-bold leading-10">Wicked</h1>
+        </div>
+        <div className="flex items-center gap-0.5">
+          <Star size={28} strokeWidth={0} fill="#FDE047" />
+          <div className="flex items-center">
+            <p className="text-neutral-50 text-lg font-semibold leading-7">
+              6.9
+            </p>
+            <p className="text-neutral-50 opacity-50 text-base font-normal leading-6">
+              /10
+            </p>
+          </div>
+        </div>
+        <p className="text-neutral-50 text-s font-normal leading-5">
+          Elphaba, a misunderstood young woman because of her green skin, and
+          Glinda, a popular girl, become friends at Shiz University in the Land
+          of Oz. After an encounter with the Wonderful Wizard of Oz, their
+          friendship reaches a crossroads.
+        </p>
+        <Button variant="outline">
+          <Play />
+          <p>Watch Trailer</p>
+        </Button>
+      </div>
     </Carousel>
   );
 };
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,7 +5,7 @@ import { Search } from "./Search";
 
 export const Header = () => {
   return (
-    <div className="flex bg-amber-100 justify-between items-center w-full max-w-7xl m-auto">
+    <div className="flex bg-white justify-between items-center w-full max-w-7xl m-auto">
       <div className="flex gap-2">
         <Film size={20} strokeWidth={1} className="text-indigo-700" />
         <p className="text-indigo-700 text-base font-bold leading-5 tracking-tight ">
