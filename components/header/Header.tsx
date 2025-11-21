febbede3ce51ddc2b66bf52c5f1afd94a77fa4ba
Tom <tomor.enkh@gmail.com>
2025-11-21 17:52:37 +0800

    Link the header logo to the home page

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,14 +2,20 @@ import { Film } from "lucide-react";
 import { FilterGenres } from "./FilterGenres";
 import { ModeToggle } from "./ModeToggle";
 import { Search } from "./Search";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className="flex justify-between items-center w-full max-w-7xl h-16 m-auto">
-      <div className="flex gap-2">
-        <Film size={20} strokeWidth={1} />
-        <p className="text-base font-bold leading-5 tracking-tight ">Movie Z</p>
-      </div>
+      <Link rel="preload" href={`/`}>
+        <div className="flex gap-2">
+          <Film size={20} strokeWidth={1} />
+          <p className="text-base font-bold leading-5 tracking-tight ">
+            Movie Z
+          </p>
+        </div>
+      </Link>
+
       <div className="flex gap-3">
         <FilterGenres />
         <Search />
